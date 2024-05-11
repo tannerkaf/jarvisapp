657cbf5300fcba11945ff7a4ef8ecb713ae412f2
tannerkaf <tannerhk04@gmail.com>
2024-05-11 18:32:15 -0500

    wsndvj

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ function openTab(evt, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+
+    if (tabName === 'ObjectDetection') {
+        initializeCamera();
+    }
+}
+
+function initializeCamera() {
+    const video = document.getElementById('webcam');
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function(stream) {
+                video.srcObject = stream;
+            })
+            .catch(function(error) {
+                console.error('Error accessing the webcam:', error);
+            });
+    }
 }
 
 function appendMessage(sender, message) {
